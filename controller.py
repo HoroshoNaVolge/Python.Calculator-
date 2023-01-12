15319737ff4908a91bd2535192c800a4814ef46b
HoroshoNaVolge <44640207+HoroshoNaVolge@users.noreply.github.com>
2023-01-12 17:04:31 +0300

    add logger

--- controller.py
+++ controller.py
@@ -2,8 +2,11 @@
 
 import view
 import operations
+import log
 
 def button_click():
     value_lst, kind_oper = view.get_value()
+    log.logwrite("Ввод: ", value_lst) 
     result = operations.calc(value_lst, kind_oper)
+    log.logwrite("Результат: ", result)
     view.output(result)
